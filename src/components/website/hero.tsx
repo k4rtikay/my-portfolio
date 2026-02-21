3616d16066890dfaf9ac801de863b70ef1b789d9
k4rtikay <katt1bh4i@gmail.com>
2026-02-21 13:25:35 +0000

    style: spacing

--- src/components/website/hero.tsx
+++ src/components/website/hero.tsx
@@ -3,8 +3,8 @@
 export function Hero() {
     return (
         <section>
-            <h1 className="text-2xl sm:text-xl font-semibold mb-2">Kartikeya Mishra</h1>
-            <p className="text-lg text-foreground leading-relaxed">
+            <h1 className="text-2xl sm:text-xl font-semibold mb-1">Kartikeya Mishra</h1>
+            <p className="text-foreground leading-relaxed">
                 Crafting interfaces and experiences.
             </p>
         </section>
