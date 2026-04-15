681c1c59a4b0e2a098086ef0cbe4be5d643e9c8e
k4rtikay <katt1bh4i@gmail.com>
2026-04-15 13:08:22 +0000

    feat: add profile picture

--- src/components/website/about.tsx
+++ src/components/website/about.tsx
@@ -1,9 +1,9 @@
 export function About() {
     return (
         <section>
-            <h2 className="text-base sm:text-sm font-medium text-muted-foreground tracking-wide mb-4">
+            {/* <h2 className="text-base sm:text-sm font-medium text-muted-foreground tracking-wide mb-4">
                 About
-            </h2>
+            </h2> */}
             <div className="space-y-4 text-base text-muted-foreground sm:text-sm tracking-wide leading-relaxed">
                 <p>
                     I&apos;m a <span className="text-foreground">full-stack developer</span> who cares deeply about design, performance, and thoughtful interactions. I build web experiences <span className="text-foreground">end to end</span> - from UI/UX to deployment.
--- src/components/website/hero.tsx
+++ src/components/website/hero.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
+
 export function Hero() {
     return (
-        <section>
-            <h1 className="text-2xl sm:text-xl font-semibold mb-1">Kartikeya Mishra</h1>
-            <p className="text-foreground leading-relaxed">
-                Crafting interfaces and experiences.
-            </p>
+        <section className="flex items-center gap-4">
+            <Image
+                src="/wa-pfp.jpg"
+                alt="Hero"
+                width={100}
+                height={100}
+                className="rounded-xl p-0.5 w-14 h-14 shadow-[0_4px_10px_0_rgba(0,0,0,0.5)] border border-border object-cover rotate-0 hover:scale-110 hover:rotate-2 hover:shadow-xl transition-all duration-200 ease-in-out"
+            />
+            <div>
+                <h1 className="text-2xl sm:text-xl font-semibold mb-1">Kartikeya Mishra</h1>
+            </div>
         </section>
     );
 }
