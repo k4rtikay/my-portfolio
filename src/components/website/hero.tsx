63d6487daef7e42be6cad4258d21068b6d78c8f8
k4rtikay <katt1bh4i@gmail.com>
2026-04-21 18:47:25 +0000

    style: pfp shadow

--- src/components/website/hero.tsx
+++ src/components/website/hero.tsx
@@ -8,7 +8,7 @@ export function Hero() {
                 alt="Hero"
                 width={100}
                 height={100}
-                className="rounded-xl p-0.5 w-14 h-14 shadow-[0_4px_10px_0_rgba(0,0,0,0.5)] border border-border object-cover rotate-0 hover:scale-110 hover:rotate-2 hover:shadow-xl transition-all duration-200 ease-in-out"
+                className="rounded-xl p-0.5 w-14 h-14 shadow-[0_4px_10px_0_rgba(0,0,0,0.25)] border border-border object-cover rotate-0 hover:scale-110 hover:rotate-2 hover:shadow-[0_8px_16px_0_rgba(0,0,0,0.2)] transition-all duration-200 ease-in-out"
             />
             <div>
                 <h1 className="text-2xl sm:text-xl font-semibold mb-1">Kartikeya Mishra</h1>
