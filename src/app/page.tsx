c2b21c02f81f222b86cb11fc61eb6af5b3e25faa
k4rtikay <katt1bh4i@gmail.com>
2026-06-07 06:47:44 +0000

    feat: add a background gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Header } from "@/components/website/header";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen w-full">
+      <div className="absolute top-0 z-[-2] h-screen w-full bg-background bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
       <Header />
       <main className="mx-auto w-full max-w-[650px] px-8 sm:px-6 py-24 md:py-32 flex-1">
         <FadeIn delay={0}>
@@ -15,9 +16,9 @@ export default function Home() {
             </div>
           </div>
         </FadeIn>
-        <FadeIn delay={0.1}>
+        {/* <FadeIn delay={0.1}>
           <Bento />
-        </FadeIn>
+        </FadeIn> */}
         <FadeIn delay={0.2}>
           <Projects />
         </FadeIn>
